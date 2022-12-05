208d679910c3cdc5e196796ccd203e6e2cb1ef18
Antonio-Italiano <antoitalia221@gmail.com>
2022-12-05 15:42:14 +0100

    Add User Information

--- js/script.js
+++ js/script.js
@@ -18,8 +18,15 @@ const pathwayElement = document.getElementById('pathway');
 const priceElement = document.getElementById('price');
 // console.log(ageElement, pathwayElement, priceElement)
 
-// 2 - generare il prezzo standard per Km
+// 2 - generare il prezzo standard per Km 
+// 3 - generare gli sconti applicati
 const price = 0.21;
 const price_minor = price - 0.042 ;
 const price_hover65 = price - 0.084 ;
 // console.log(price, price_minor, price_hover65)
+
+// 4 - chiedere all'utente di inserire la sua Età e distanza
+const yourAge = prompt('Inserisci la tua età', 20);
+const yourPathway = prompt('Inserisci la distanza della tratta in km', 20);
+// console.log(yourAge, yourPathway);
+
